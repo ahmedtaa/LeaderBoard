a5e79598062e6b87757515d98cf63920df93beb8
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-01-27 20:54:48 +0200

    inhance the UI

--- src/render.js
+++ src/render.js
@@ -6,14 +6,25 @@ const renderList = (list) => {
     container.innerHTML = '<li>NO SCORES YET</li>';
     return;
   }
-
+  let counter = 0;
   list
     .sort((a, b) => b.score - a.score)
     .forEach((score, index) => {
       const li = document.createElement('li');
-      li.textContent = `${score.user}: ${score.score}`;
-      li.id = index;
 
+      li.id = index;
+      const user = document.createElement('span');
+      user.className = 'user';
+      user.textContent = `${score.user}`;
+      const order = document.createElement('span');
+      order.className = 'order';
+      order.textContent = `${(counter += 1)}`;
+      const span = document.createElement('span');
+      span.className = 'score';
+      span.textContent = `${score.score}`;
+      li.appendChild(order);
+      li.appendChild(user);
+      li.appendChild(span);
       container.append(li);
     });
 };
